8aad2b37f9f5e6a257e8cade23d9bb37c0a19c0f
madhavmanohars <madhav.ms@inapp.com>
2018-09-17 12:31:17 +0530

    Send username over websocket

--- BLOG_PROJECT/chat/consumers.py
+++ BLOG_PROJECT/chat/consumers.py
@@ -4,6 +4,7 @@ import json
 
 class ChatConsumer(AsyncWebsocketConsumer):
     async def connect(self):
+        self.user = self.scope["user"]
         self.room_name = self.scope['url_route']['kwargs']['room_name']
         self.room_group_name = 'chat_%s' % self.room_name
 
@@ -26,21 +27,23 @@ class ChatConsumer(AsyncWebsocketConsumer):
     async def receive(self, text_data):
         text_data_json = json.loads(text_data)
         message = text_data_json['message']
-
+        self.scope["session"].save()
         # Send message to room group
         await self.channel_layer.group_send(
             self.room_group_name,
             {
                 'type': 'chat_message',
-                'message': message
+                'message': message,
+                'username': self.scope["user"].username,
             }
         )
 
     # Receive message from room group
     async def chat_message(self, event):
-        message = event['message']
+        message = event['message'],
 
         # Send message to WebSocket
         await self.send(text_data=json.dumps({
-            'message': message
+            'message': message,
+            "username": event["username"],
         }))
